20d98a1cef4bc3fd7f75aa6ec0d88d3a40d501b7
IFPI <aluno@ifpi.edu.br>
2022-10-06 19:28:46 -0300

    nada

--- banco/src/Cliente.java
+++ banco/src/Cliente.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.util.Date;
 
 public class Cliente {
     private String nome;
@@ -6,27 +7,36 @@ public class Cliente {
     private LocalDate dataNasc;
     private String endereco;
 
-    public Cliente(String nome, String cpf, LocalDate dataNasc, String endereco) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.dataNasc = dataNasc;
-        this.endereco = endereco;
-    }
 
+
+    public Cliente(String string, String string2, Date date, String string3) {
+    }
     public String getNome() {
         return nome;
     }
-
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
     public String getCpf() {
         return cpf;
     }
-
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
     public LocalDate getDataNasc() {
         return dataNasc;
     }
-
+    public void setDataNasc(LocalDate dataNasc) {
+        this.dataNasc = dataNasc;
+    }
     public String getEndereco() {
         return endereco;
     }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
     
+
+
 }
--- banco/src/Conta.java
+++ banco/src/Conta.java
@@ -3,39 +3,26 @@ public class Conta {
     private int numConta;
     private double saldo;
 
-    public Conta(int numAgencia, int numConta, float saldo){
-        this.numAgencia = numAgencia;
-        this.numConta = numConta;
-        this.saldo = saldo;
-        
+
+
+    public Conta(int i, int j, int k) {
     }
-
-
     public int getNumAgencia() {
         return numAgencia;
     }
-
+    public void setNumAgencia(int numAgencia) {
+        this.numAgencia = numAgencia;
+    }
     public int getNumConta() {
         return numConta;
     }
-
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
     public double getSaldo() {
-        saldo = 120.50;
         return saldo;
     }
-        //o saldo de sua conta, depositar, sacar e transferir
-    void mostrarSaldo(double saldo){
-        System.out.println(saldo);
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
     }
-    void saque(double valor){
-        double saldoNovo = this.saldo - valor;
-        this.saldo = saldoNovo;
-    }
-    void deposito(double quantia){
-        double saldoNovo = this.saldo + quantia;
-        this.saldo = saldoNovo;
-    }
-       
-    
-    
 }
