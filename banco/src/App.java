5c1461c8cef152e4b1090797b37843d78f3a6048
Marciely Ferreira <89710284+marcielysilva@users.noreply.github.com>
2022-10-17 21:34:29 -0300

    atualizacao do app

--- banco/src/App.java
+++ banco/src/App.java
@@ -1,10 +1,11 @@
 import java.text.SimpleDateFormat;
 public class App {
+    private String Conta;
+    private String Cliente;
     
     public static void main(String[] args) throws Exception {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
        
-
         Cliente c1 = new Cliente("Ezequias","12312312323",sdf.parse("20/06/2000"), "rua 12");
         Conta conta1 = new Conta(1, 1, 1);
       
